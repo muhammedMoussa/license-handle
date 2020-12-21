28072d6a023b6c7e01600d4e2c89025d28dce9d2
Muhammed <muhammedmoussa@hotmail.com>
2020-12-21 18:57:36 +0200

    handle cors

--- functions/license.js
+++ functions/license.js
@@ -8,6 +8,12 @@ exports.handler = async () => {
    
     return {
       statusCode: 200,
+      headers: {
+        /* Required for CORS support to work */
+        'Access-Control-Allow-Origin': '*',
+        /* Required for cookies, authorization headers with HTTPS */
+        'Access-Control-Allow-Credentials': true
+      },
       body: JSON.stringify({ data: 'data' })
     };
   };
