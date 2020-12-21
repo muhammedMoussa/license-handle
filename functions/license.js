25d4ffe3ca91a32e1c3950a36842308538a23a30
Muhammed <muhammedmoussa@hotmail.com>
2020-12-21 19:06:50 +0200

    update res

--- functions/license.js
+++ functions/license.js
@@ -1,14 +1,17 @@
 exports.handler = async () => {
-    // if (errors) {
-    //   return {
-    //     statusCode: 500,
-    //     body: JSON.stringify(errors)
-    //   };
-    // }
+    if (errors) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(errors)
+      };
+    }
    
     return {
       statusCode: 200,
-      body: JSON.stringify({ data: 'data' }),
+      body: JSON.stringify({ 
+        PersonnelUrl:"https://payroll.hadaf.website/api",
+        IdentityUrl:"https://accesscontrol.hadaf.website"
+      }),
       headers: {
         "access-control-allow-origin": "*"
       }
